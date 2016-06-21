5bba3f482b366c01de3569a609b24f21aae03246
ali <alisaleh65@yahoo.com>
2016-06-21 18:40:17 +0430

    Add Join model, and some setting in admin for showing it.

--- joins/admin.py
+++ joins/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Join
+
+
+class JoinAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'email', 'timestamp', 'updated']
+
+    class Meta:
+        model = Join
+
+admin.site.register(Join, JoinAdmin)
--- joins/models.py
+++ joins/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Join(models.Model):
+
+    email = models.EmailField()
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return "Joined : " + str(self.email)
