627bcdff34c7ccb04c4381144b8d64c2b366fa50
ali <alisaleh65@yahoo.com>
2016-06-21 19:41:45 +0430

    Add Join Form.

--- joins/views.py
+++ joins/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from .forms import EmailForm, JoinForm
+from .models import Join
 
 
-# Create your views here.
 def home(request):
+    #
+    # form = EmailForm(request.POST or None)
+    # if form.is_valid():
+    #
+    #     email = form.cleaned_data['email']
+    #     new_join, created = Join.objects.get_or_create(email=email)
+    #     print("ok email :) ", new_join, created)
+    #
+    #     if created:
+    #         print("this email was created")
 
-    context = {}
+    form = JoinForm(request.POST or None)
+
+    if form.is_valid():
+        new_join = form.save(commit=False)
+        # we might do something.
+        new_join.save()
+
+    context = {'form': form}
     template = 'home.html'
 
     return render(request, template, context)
